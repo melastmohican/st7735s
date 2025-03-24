b3dfee5346a3910aeb2fda7ff8594ab2ee465b1b
Mariusz Jurgielewicz <mordor@mail.com>
2025-03-23 19:58:24 -0700

    Example projects

---
+++ examples/pico-example/src/main.rs
@@ -0,0 +1,138 @@
+//! Draw a image. On a 80x160 ST7735 display over SPI.
+//!
+//! This example is for the Raspberry Pi Pico board using SPI0.
+//!
+//! Wiring connections are as follows for  display:
+//!
+//! ```
+//! LCD | => | Pico
+//! ----|----|-----
+//! VCC | -> | VSYS
+//! GND | -> | GND
+//! DIN | -> | 19
+//! CLK | -> | 18
+//! CS | -> | 9
+//! DC | -> | 8
+//! RST | -> | 12
+//! BL | -> | 13
+//! ```
+//!
+//! Run on a Rpi Pico with `cargo run --example st7735s`.
+
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use panic_probe as _;
+
+use bsp::entry;
+
+use bsp::hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog};
+use cortex_m::asm::nop;
+use embedded_graphics::image::ImageRawLE;
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    prelude::*,
+};
+use embedded_hal::digital::OutputPin;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use rp_pico as bsp;
+use rp_pico::hal::fugit::RateExtU32;
+use rp_pico::hal::gpio::FunctionSpi;
+use rp_pico::hal::{spi, Clock};
+use tinybmp::Bmp;
+use st7735s::{Orientation, PixelOrder, ST7735};
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+        .ok()
+        .unwrap();
+
+    //let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut delay = DelayCompat(cortex_m::delay::Delay::new(
+        core.SYST,
+        clocks.system_clock.freq().to_Hz(),
+    ));
+
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let sck = pins.gpio18.into_function::<FunctionSpi>(); // SCK
+    let mosi = pins.gpio19.into_function::<FunctionSpi>(); // SCL TX
+    let miso = pins.gpio16.into_function::<FunctionSpi>(); // SDA RX
+
+    let dc = pins.gpio8.into_push_pull_output();
+    let cs = pins.gpio9.into_push_pull_output();
+    let rst = pins.gpio12.into_push_pull_output();
+    let mut bl = pins.gpio13.into_push_pull_output();
+
+    // Create an SPI driver instance for the SPI0 device
+    let spi = spi::Spi::<_, _, _, 8>::new(pac.SPI0, (mosi, miso, sck)).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        16_000_000u32.Hz(),
+        embedded_hal::spi::MODE_0,
+    );
+
+    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+    let mut display = ST7735::new(
+        spi_dev,
+        dc,
+        rst,
+        PixelOrder::BGR,
+        true,
+        Orientation::LandscapeSwapped,
+        160,
+        80,
+    );
+    display.init(&mut delay);
+    display.set_orientation(Orientation::LandscapeSwapped);
+    display.clear(Rgb565::BLUE);
+
+    // draw rust logo
+    let logo = Bmp::from_slice(include_bytes!("rust.bmp")).unwrap();
+    let logo = Image::new(&logo, Point::new(40, 0));
+    logo.draw(&mut display).unwrap();
+
+    bl.set_high().ok();
+
+    loop {
+        nop()
+    }
+}
+
+struct DelayCompat(cortex_m::delay::Delay);
+impl embedded_hal::delay::DelayNs for DelayCompat {
+    fn delay_ns(&mut self, mut ns: u32) {
+        while ns > 1000 {
+            self.0.delay_us(1);
+            ns = ns.saturating_sub(1000);
+        }
+    }
+
+    fn delay_us(&mut self, us: u32) {
+        self.0.delay_us(us);
+    }
+}
---
+++ examples/stm32h7-example/src/main.rs
@@ -0,0 +1,132 @@
+#![no_std]
+#![no_main]
+
+use panic_probe as _;
+use cortex_m_rt::entry;
+use embedded_graphics::mono_font::ascii::FONT_6X9;
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::text::Text;
+use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
+use embedded_graphics::image::Image;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_hal_compat::ForwardCompat;
+use st7735s::{Orientation, PixelOrder, ST7735};
+use stm32h7xx_hal::hal::spi;
+use stm32h7xx_hal::{prelude::*, spi::NoMiso, stm32};
+use tinybmp::Bmp;
+
+struct Led<P: OutputPin> {
+    pin: P,
+    brightness: u8, // Duty cycle (0-10)
+}
+
+impl<P: OutputPin> Led<P> {
+    pub fn new(pin: P) -> Self {
+        Self {
+            pin,
+            brightness: 5, // Default 50% duty cycle
+        }
+    }
+
+    pub fn set_brightness(&mut self, value: u8) {
+        if value <= 10 {
+            self.brightness = value;
+        }
+    }
+
+    pub fn update<DELAY>(&mut self, delay: &mut DELAY)
+    where
+        DELAY: DelayNs,
+    {
+
+        for count in 0..10 {
+            if count < self.brightness {
+                self.pin.set_high().ok();
+            } else {
+                self.pin.set_low().ok();
+            }
+            delay.delay_ms(1); // Adjust for timing
+        }
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc
+        .sys_ck(96.MHz())
+        .pll1_q_ck(48.MHz())
+        .freeze(pwrcfg, &dp.SYSCFG);
+
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
+    // SPI4
+    let sck = gpioe.pe12.into_alternate();
+    let mosi = gpioe.pe14.into_alternate();
+
+    let mut rst = gpioe.pe15.into_push_pull_output().forward();
+    let mut dc = gpioe.pe13.into_push_pull_output().forward();
+    let mut cs = gpioe.pe11.into_push_pull_output().forward();
+    let mut led = Led::new(gpioe.pe10.into_push_pull_output().forward());
+
+
+    // Initialise the SPI peripheral.
+    let spi = dp
+        .SPI4
+        .spi(
+            (sck, NoMiso, mosi),
+            spi::MODE_0,
+            3.MHz(),
+            ccdr.peripheral.SPI4,
+            &ccdr.clocks,
+        )
+        .forward();
+
+    //let mut delay = cp.SYST.delay(ccdr.clocks).forward();
+    let mut delay = cortex_m::delay::Delay::new(cp.SYST, ccdr.clocks.sysclk().raw()).forward();
+
+    led.update(&mut delay);
+
+
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+    let mut display = ST7735::new(
+        spi_device,
+        dc,
+        rst,
+        PixelOrder::BGR,
+        true,
+        Orientation::LandscapeSwapped,
+        160,
+        80,
+    );
+    display.init(&mut delay);
+    display.set_orientation(Orientation::LandscapeSwapped);
+    display.clear(Rgb565::BLUE);
+
+    let style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X9)
+        .text_color(Rgb565::RED)
+        .background_color(Rgb565::GREEN)
+        .build();
+
+    Text::new(
+        "This is a\nmultiline\nHello World!",
+        Point::new(30, 30),
+        style,
+    )
+        .draw(&mut display).unwrap();
+
+    let logo = Bmp::from_slice(include_bytes!("rust.bmp")).unwrap();
+    let logo = Image::new(&logo, Point::new(40, 0));
+    logo.draw(&mut display).unwrap();
+
+    loop {}
+}
